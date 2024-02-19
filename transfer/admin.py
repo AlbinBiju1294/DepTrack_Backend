8bf44439262f2b2fb25df159d666b683322d6738
Albin Biju <albin.biju@experionglobal.local>
2024-02-20 00:25:25 +0530

    feat: added endpoints for creating, listing, filtering transfers

--- transfer/admin.py
+++ transfer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Transfer,TransferDetails
+
+
+# Register your models here.
+admin.site.register(TransferDetails)
+admin.site.register(Transfer)
